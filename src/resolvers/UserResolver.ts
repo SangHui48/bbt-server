993646b9071146e186f15110dad861cad457e976
yogle <qjzjsldj@gmail.com>
2021-03-21 16:24:48 +0900

    로그인 기능 추가

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -45,4 +45,19 @@ export class UserResolver {
         return User.findOne({id:id});
     }   
 
+    @Mutation(() =>Boolean)
+    async login( @Arg('data') data: UserInput ): Promise<Boolean> {
+        const { id, pw } = data;
+        const user = await User.findOne({ where: { id } });
+        if (!user) {
+            throw new Error(`The user with id: ${id} does not exist!`);
+        }
+        // 비밀번호 확인
+        const valid = await bcrypt.compare(pw,user.pw);
+        if (!valid) {
+            throw new Error(`Password not mached!`);
+        }
+        // accessToken 발급
+        return true;
+    }
 }
